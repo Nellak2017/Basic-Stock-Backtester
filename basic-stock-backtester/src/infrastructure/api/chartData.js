0c769d9b87b27d9a9b9132ae4d8f876cce4e92b7
Connor Keenum <Nellakprime@gmail.com>
2022-05-14 11:51:35 -0500

    Made a simple Axios API caller, that calls my Flask Backend.

--- basic-stock-backtester/src/infrastructure/api/chartData.js
+++ basic-stock-backtester/src/infrastructure/api/chartData.js
@@ -1,5 +1,24 @@
+import axios from 'axios';
 // Call the API to get the Chart data. Pass it through the data transformer and return it to the caller.
 
 // 1. Call the API to get the data
 // 2. Pass it through the data transformer
 // 3. Return that transformed data to the caller
+
+export const getChartData = (ticker, interval, period, upperSell, lowerSell, initHolding,
+    strategy, lowerIndicator, upperIndicator) => {
+        const URL = `http://localhost:5000/${strategy}?ticker=${ticker}&interval=${interval}&period=${period}&upperSell=${upperSell}&lowerSell=${lowerSell}&initHolding=${initHolding}&lowerIndicator=${lowerIndicator}&upperIndicator=${upperIndicator}`
+        axios.get(URL, {
+            headers: {
+                'content-type': 'application/json'
+            }
+        })
+        .then(response => response)
+        .catch( error => {
+            console.error(error)
+        })
+        .then( function () {
+            console.log(`Input data --> ticker: ${ticker}, interval: ${interval}, period: ${period}, upperSell: ${upperSell}, lowerSell: ${lowerSell}, 
+            initHolding: ${initHolding}, lowerIndicator: ${lowerIndicator}, upperIndicator: ${upperIndicator}`)
+        });
+}
