59e00474c9021684dfbaa491760453fa4a7a6c1e
Connor Keenum <Nellakprime@gmail.com>
2022-05-21 21:56:00 -0500

    Experimented with async/await syntax, went back to .then syntax. Wondering how to test this API function.

--- basic-stock-backtester/src/infrastructure/api/chartData.js
+++ basic-stock-backtester/src/infrastructure/api/chartData.js
@@ -1,4 +1,6 @@
 import axios from 'axios';
+import axiosRetry from 'axios-retry';
+import { DEFAULT_RETRIES } from '../content/constants';
 // Call the API to get the Chart data. Pass it through the data transformer and return it to the caller.
 
 // 1. Call the API to get the data
@@ -8,26 +10,30 @@ import axios from 'axios';
 // Todo: Clean this up, by simplifying the props passed
 // Todo: Improve error handling
 // Todo: remove unused .then
-export const getChartData = (data, res, err) => {
-        const {ticker, interval, period, upperSell, lowerSell, initHolding,
-            strategy, lowerIndicator, upperIndicator} = data;
-        const URL = `http://localhost:5000/${strategy}?ticker=${ticker}&interval=${interval}&period=${period}&upperSell=${upperSell}&lowerSell=${lowerSell}&initHolding=${initHolding}&lowerIndicator=${lowerIndicator}&upperIndicator=${upperIndicator}`
-        axios.get(URL, {
-            headers: {
-                'content-type': 'application/json'
-            }
-        })
+
+export const getChartData = async (data, res, err) => {
+    const { ticker, interval, period, upperSell, lowerSell, initHolding,
+        strategy, lowerIndicator, upperIndicator } = data;
+    axiosRetry(axios, { retries: DEFAULT_RETRIES, retryDelay: axiosRetry.exponentialDelay })
+    const URL = `http://localhost:5000/${strategy}?ticker=${ticker}&interval=${interval}&period=${period.toLowerCase()}&upperSell=${upperSell}&lowerSell=${lowerSell}&initHolding=${initHolding}&lowerIndicator=${lowerIndicator}&upperIndicator=${upperIndicator}`
+    axios.get(URL, {
+        headers: {
+            'content-type': 'application/json',
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Credentials': true
+        }, timeout: 8000
+    })
         .then(response => {
-            if(res != null) {
+            if (res != null) {
                 res(response)
             }
         })
-        .catch( error => {
-            if(err != null) {
+        .catch(error => {
+            if (err != null) {
                 err(error)
             }
         })
-        .then( function () {
+        .then(function () {
             console.log(`Input data --> ticker: ${ticker}, interval: ${interval}, period: ${period}, upperSell: ${upperSell}, lowerSell: ${lowerSell}, 
             initHolding: ${initHolding}, lowerIndicator: ${lowerIndicator}, upperIndicator: ${upperIndicator}`)
         });
