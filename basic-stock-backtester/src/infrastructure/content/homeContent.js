730fe81edfb9b2692978b0cbb631379340a8b114
Connor Keenum <Nellakprime@gmail.com>
2022-05-21 21:58:12 -0500

    Cleaned this up.

--- basic-stock-backtester/src/infrastructure/content/homeContent.js
+++ basic-stock-backtester/src/infrastructure/content/homeContent.js
@@ -1,11 +1,35 @@
-import faker from 'faker'; // todo: Import Data from API instead
+// This file contains any content or data that will be on the front end
+import {
+    ONE_DAY,
+    ONE_WEEK,
+    ONE_MONTH,
+    THREE_MONTHS,
+    ONE_YEAR,
+    FIVE_YEARS,
+    ALL,
+    ONE_MINUTE,
+    TWO_MINUTES,
+    FIVE_MINUTES,
+    FIFTEEN_MINUTES,
+    THIRTY_MINUTES,
+    NINETY_MINUTES,
+    ONE_HOUR,
+    FIVE_DAYS,
+    DEFAULT_TICKER,
+    UPPER_SELL,
+    LOWER_SELL,
+    INIT_HOLDING,
+    STRATEGY,
+    EMA_LOWER_INDICATOR,
+    EMA_UPPER_INDICATOR
+} from '../content/constants'
 
 export const formInputs = [
     {
         id: 1,
         name: "ticker",
         type: "text",
-        placeholder: "ETH-USD",
+        placeholder: DEFAULT_TICKER,
         label: "Stock Ticker",
         element: "input",
         required: true
@@ -14,30 +38,30 @@ export const formInputs = [
         id: 2,
         name: "interval",
         type: "select",
-        placeholder: "1M",
+        placeholder: ONE_MONTH,
         label: "Query Interval",
         element: "select",
         required: true,
         options: [
-            {option: "1m"},
-            {option: "2m"},
-            {option: "5m"},
-            {option: "15m"},
-            {option: "30m"},
-            {option: "90m"},
-            {option: "1h"},
-            {option: "1d"},
-            {option: "5d"},
-            {option: "1wk"},
-            {option: "1mo"},
-            {option: "3mo"},
+            { option: ONE_MINUTE },
+            { option: TWO_MINUTES },
+            { option: FIVE_MINUTES },
+            { option: FIFTEEN_MINUTES },
+            { option: THIRTY_MINUTES },
+            { option: NINETY_MINUTES },
+            { option: ONE_HOUR },
+            { option: ONE_DAY },
+            { option: FIVE_DAYS },
+            { option: ONE_WEEK },
+            { option: ONE_MONTH },
+            { option: THREE_MONTHS },
         ]
     },
     {
         id: 3,
         name: "upperSell",
         type: "number",
-        placeholder: "1.10",
+        placeholder: UPPER_SELL,
         label: "Upper Sell",
         element: "input",
         required: true
@@ -46,7 +70,7 @@ export const formInputs = [
         id: 4,
         name: "lowerSell",
         type: "number",
-        placeholder: ".95",
+        placeholder: LOWER_SELL,
         label: "Lower Sell",
         element: "input",
         required: true
@@ -60,8 +84,8 @@ export const formInputs = [
         element: "select",
         required: true,
         options: [
-            {option: "Yes"},
-            {option: "No"},
+            { option: "Yes" },
+            { option: "No" },
         ]
     },
     {
@@ -73,11 +97,11 @@ export const formInputs = [
         element: "select",
         required: true,
         options: [
-            {option: "Conservative Momentum"},
-            {option: "Adaptive Momentum"},
-            {option: "Dollar Cost Averaging"},
-            {option: "HODL"},
-            {option: "Value Investing"},
+            { option: "Conservative Momentum" },
+            { option: "Adaptive Momentum" },
+            { option: "Dollar Cost Averaging" },
+            { option: "HODL" },
+            { option: "Value Investing" },
         ]
     },
     {
@@ -89,8 +113,8 @@ export const formInputs = [
         element: "select",
         required: true,
         options: [
-            {option: "EMA-12"},
-            {option: "SMA-12"},
+            { option: "EMA-12" },
+            { option: "SMA-12" },
         ]
     },
     {
@@ -102,8 +126,8 @@ export const formInputs = [
         element: "select",
         required: true,
         options: [
-            {option: "EMA-24"},
-            {option: "SMA-24"},
+            { option: "EMA-24" },
+            { option: "SMA-24" },
         ]
     },
 ];
@@ -111,71 +135,73 @@ export const formInputs = [
 export const chartStockPeriods = [
     {
         id: 1,
-        value: "1D"
+        value: ONE_DAY
     },
     {
         id: 2,
-        value: "1W"
+        value: ONE_WEEK
     },
     {
         id: 3,
-        value: "1M"
+        value: ONE_MONTH
     },
     {
         id: 4,
-        value: "3M"
-    }, 
+        value: THREE_MONTHS
+    },
     {
         id: 5,
-        value: "1Y"
+        value: ONE_YEAR
     },
     {
         id: 6,
-        value: "5Y"
+        value: FIVE_YEARS
     },
     {
         id: 7,
-        value: "ALL"
+        value: ALL
     },
 ];
 
-export const range = (start, end) => {
-    return Array(end - start + 1).fill().map((_, idx) => start + idx)
-}
-
-// Todo: make this method actually return the correct value
-export const daysInterval = (intervalStr) => {
-    if (intervalStr.toUpperCase() === "1D"){
-        return 1
-    } else if (intervalStr.toUpperCase() === "1W") {
-        return 7
-    } else if (intervalStr.toUpperCase() === "1M") {
-        return 31
-    } else if (intervalStr.toUpperCase() === "1Y") {
-        return 365
-    }else if (intervalStr.toUpperCase() === "5Y") {
-        return 5*365
-    }
-    else{
-        return 5*365
-    }
-}
-
-export const randomColor = () => {
-    return Math.floor((Math.random() * 255) + 1)
-}
-
-export const extractData = (lis, value) => {
-    if (lis !== null){
-        let dataPoints = []
-        for (let dataPoint of lis){
-          dataPoints.push(dataPoint[value]);
-        }
-        return dataPoints
-    }
-}
-
 export const chartLabels = ["value"]
+
+export const defaultStockIntervals = {
+    ONE_DAY: ONE_MINUTE,
+    ONE_WEEK: FIVE_MINUTES,
+    ONE_MONTH: FIFTEEN_MINUTES,
+    THREE_MONTHS: ONE_HOUR,
+    ONE_YEAR: ONE_DAY,
+    FIVE_YEARS: ONE_WEEK,
+    ALL: ONE_MONTH
+}
+
+export const defaultAPIData = {
+    ONE_DAY: [],
+    ONE_WEEK: [],
+    ONE_MONTH: [],
+    THREE_MONTHS: [],
+    ONE_YEAR: [],
+    FIVE_YEARS: [],
+    ALL: []
+}
+
+export const defaultStock = {
+    "ticker": DEFAULT_TICKER,
+    "interval": ONE_MINUTE,
+    "upperSell": UPPER_SELL,
+    "lowerSell": LOWER_SELL,
+    "initHolding": INIT_HOLDING,
+    "strategy": STRATEGY,
+    "lowerIndicator": EMA_LOWER_INDICATOR,
+    "upperIndicator": EMA_UPPER_INDICATOR,
+    "period": ONE_DAY,
+}
+
+export const colors = {
+    "values": "Black",
+    "ema24": "Red",
+    "ema12": "Green"
+}
 
 export const options = {
     responsive: true,
@@ -184,40 +210,21 @@ export const options = {
             position: 'top',
         },
         title: {
-            display: true,
-            text: 'Chart.js Line Chart',
+            display: false,
+            text: '',
         },
     },
 };
 
-// This data will be from the API endpoint
-export const chartData = (dataSetLabels, labels, data) => {
-    return {
-        labels,
-        datasets: [
-            ...dataSetLabels.map((dataSetLabel) => (
-                {
-                    label: dataSetLabel,
-                    data: data[dataSetLabel],
-                    borderColor: `rgb(${randomColor()}, ${randomColor()}, ${randomColor()})`,
-                    backgroundColor: `rgba(${randomColor()}, ${randomColor()}, ${randomColor()}, 0.5)`,
-                }
-            ))
-        ],
-    }
-};
-/**
-        
-            {
-                label: 'Dataset 1',
-                data: labels.map(() => faker.datatype.number({ min: -1000, max: 1000 })),
-                borderColor: 'rgb(255, 99, 132)',
-                backgroundColor: 'rgba(255, 99, 132, 0.5)',
-            },
-            {
-                label: 'Dataset 2',
-                data: labels.map(() => faker.datatype.number({ min: -1000, max: 1000 })),
-                borderColor: 'rgb(53, 162, 235)',
-                backgroundColor: 'rgba(53, 162, 235, 0.5)',
-            },
- */
+export const defaultResData = [
+    {
+        'ticker': 'ETH-USD',
+        'date': '2022:05:12 15:30:00',
+        'value': "1939.252685546875",
+        'ema24': "1962.0431567382811",
+        'ema12': "2458.7604017991284",
+        'holding_stock': "True",
+        'current_profitability_multiplier': "1.0",
+        'position_evaluation': 'HOLD',
+        'position_two_step_evaluation': 'HOLD and HOLD'
+    }]
