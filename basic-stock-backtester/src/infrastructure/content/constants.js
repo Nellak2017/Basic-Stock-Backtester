9856364524bceb1dfedcfb45a29199d83709f42f
Connor Keenum <Nellakprime@gmail.com>
2022-05-21 21:59:07 -0500

    Updated constants.

---
+++ basic-stock-backtester/src/infrastructure/content/constants.js
@@ -0,0 +1,28 @@
+// This file contains any constants that will be on the front end
+
+export const ONE_DAY = "1D"
+export const ONE_WEEK = "1W"
+export const ONE_MONTH = "1M"
+export const THREE_MONTHS = "3M"
+export const ONE_YEAR = "1Y"
+export const FIVE_YEARS = "5Y"
+export const ALL = "ALL"
+
+export const ONE_MINUTE = "1m"
+export const TWO_MINUTES = "2m"
+export const FIVE_MINUTES = "5m"
+export const FIFTEEN_MINUTES = "15m"
+export const THIRTY_MINUTES = "30m"
+export const NINETY_MINUTES = "90m"
+export const ONE_HOUR = "1h"
+export const FIVE_DAYS = "5d"
+
+export const DEFAULT_TICKER = "ETH-USD"
+export const UPPER_SELL = "1.10"
+export const LOWER_SELL = ".95"
+export const INIT_HOLDING = "false"
+export const STRATEGY = "Conservative-Momentum"
+export const EMA_LOWER_INDICATOR = "EMA-24"
+export const EMA_UPPER_INDICATOR = "EMA-12"
+
+export const DEFAULT_RETRIES = 5
