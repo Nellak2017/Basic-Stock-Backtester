b076fa533d33b8f465566d5a20caca9ef6579a56
Connor Keenum <Nellakprime@gmail.com>
2022-05-31 18:11:21 -0500

    Cleaned up chartdata and added some input validation.

--- basic-stock-backtester/src/infrastructure/api/chartData.js
+++ basic-stock-backtester/src/infrastructure/api/chartData.js
@@ -1,19 +1,27 @@
-import axios from 'axios';
-import axiosRetry from 'axios-retry';
-import { DEFAULT_RETRIES } from '../content/constants';
+import axios from 'axios'
+import axiosRetry from 'axios-retry'
+import { DEFAULT_RETRIES, MATCH_TICKER } from '../content/constants'
 
-// Call the API to get the Chart data. Pass it through the data transformer and return it to the caller.
+// Call the API to get the Chart data.
 export const getChartData = async (data) => {
-    const { ticker, interval, period } = data;
-
-    const periodOne = data["period1"];
-    const periodTwo = data["period2"];
-
-    if (typeof ticker !== "string") { throw new Error("Ticker must be a string")}
-
+    // Axios Retry
+    // ----------------
     axiosRetry(axios, { retries: DEFAULT_RETRIES, retryDelay: axiosRetry.exponentialDelay })
-    const URL = `https://yh-finance.p.rapidapi.com/stock/v3/get-chart`;
-    const request = await axios.get(URL, {
+
+    // Variables
+    // ----------------
+    const { ticker, interval, period } = data
+    const periodOne = data["period1"]
+    const periodTwo = data["period2"]
+    const URL = `https://yh-finance.p.rapidapi.com/stock/v3/get-chart`
+
+    // Validate Input
+    // ----------------
+    if (!MATCH_TICKER.test(ticker) || typeof ticker !== "string") { throw new Error(`Ticker must be a string with a valid format in getChartData. Ticker: ${ticker}`)}
+
+    // Fetch Data and Retry if you fail to fetch it
+    // ----------------
+    return await axios.get(URL, {
         params: {
             interval: interval,
             symbol: ticker,
@@ -31,27 +39,4 @@ export const getChartData = async (data) => {
             'X-RapidAPI-Key': process.env.NEXT_PUBLIC_RAPIDAPI_KEY
         }, timeout: 4000
     })
-        /*
-        .then(response => {
-            if (res != null) {
-                res(response)
-            }
-        })
-        .catch(error => {
-            if (err != null) {
-                err(error)
-            }
-        })
-        
-        .then(() => {
-            console.log(
-`                 interval: ${interval}
-                 symbol: ${ticker}
-                 range: ${period}
-                 period1: ${periodOne}
-                 period2: ${periodTwo}
-                 typeOf: ${ request}`);
-        })
-        */
-       return request;
 }
