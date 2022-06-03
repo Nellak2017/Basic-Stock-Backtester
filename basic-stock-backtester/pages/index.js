751a052c4df7e340a78ae779bf2c8c3ed192b8a0
Connor Keenum <Nellakprime@gmail.com>
2022-06-03 15:56:06 -0500

    Finally got first version of my site working properly. Front End Sucessfully reaches API and correctly displays the chart data as intended onPageLoad only. I need to fix onSubmit and onPeriodChange to make it work properly.

--- basic-stock-backtester/pages/index.js
+++ basic-stock-backtester/pages/index.js
@@ -23,6 +23,13 @@ import {
 import moment from 'moment';
 
 // @ Todo: Clean up onPageLoad. Object.keys()[0] is ugly, find a more elgant way
+// @ Todo: Clean up glued together functions. You are mapping and the resultant is inconvient to use ([{}, {}, ..] vs {1D: {}, 1WK: {}})
+// @ Todo: If you are going to use the Map method of transforming  --> ([{}, {}, ..] vs {1D: {}, 1WK: {}}), then write a helper method to transform into useful form
+// @ Todo: Get the rest of the Period datas, not just one period
+// @ Todo: Write OnSubmit
+// @ Todo: Write OnPeriod Change
+// @ Todo: E2E tests
+// @ Todo: Integration Tests
 
 export default function Home() {
   ChartJS.register(
@@ -42,6 +49,7 @@ export default function Home() {
   const [Profit, setProfit] = useState(1)
   const [InMarket, setInMarket] = useState(CONSTANTS.INIT_HOLDING) // if you are invested or not
   const [APIData, setAPIData] = useState(content.defaultAPIData) // Stores literal API Response in it's totality, initially empty for each period
+  const [SMA, setSMA] = useState(content.defaultSMA) // Stores the Simple Moving Average for the 2 intervals I am considering, sma24 and sma12 for each period
   const [Displayed, setDisplayed] = useState(content.defaultDisplayed) // Shows what is on the chart (for now only works with ema12 and ema24 indicators)
   const [ChartRange, setChartRange] = useState(3600)
   const [formValues, setFormValues] = useState(content.defaultFormValues)
@@ -57,83 +65,61 @@ export default function Home() {
   // Helper Functions
   // ----------------
 
-  // Get the stock data for one period
+  // Get the stock data for one period, and store in API data object
   const getPeriod = async (ticker, period, interval) => {
     const form = { ticker, interval, period }
     const response = await getChartData(form)
-    console.log(response.data)
     setAPIData({ ...APIData, [period]: response.data })
   }
 
-  /**
-   * onPageLoad :
-   *  ~get all the period stock data and store values in APIData
-   *  (let memo run)
-   *  display visible period in line chart (Set the data so Line chart can display it)
-   *  
-   * when APIData is changed:
-   *  memoize the values and calculate the EMA values and store them too
-   * 
-   * onPeriodChange :
-   *  if there is data in the memo for this period, display it
-   *  else fetch the data and use that data, if you can't display error fetching data
-   * 
-   * onSubmit:
-   *  get all the period stock data and store values in APIData
-   *  (let memo run)
-   *  display visible period in line chart
-   */
+  // Get the SMA data for one date, and store in SMA data object
+  const getSMA = async (ticker, date, period) => {
+    const sma24 = await getSmaDataPoint(ticker, date, 24)
+    const sma12 = await getSmaDataPoint(ticker, date, 12)
+    setSMA(prevSMA => ({ ...prevSMA, [period]: { ...prevSMA[period], "sma12": typeof sma12 === "undefined" ? -1 : sma12["sma"] } }))
+    setSMA(prevSMA => ({ ...prevSMA, [period]: { ...prevSMA[period], "sma24": typeof sma24 === "undefined" ? -1 : sma24["sma"] } }))
+  }
 
   // Hooks
   // ----------------
 
   // onPageLoad, Get Default values for chart 
   useLayoutEffect(() => {
+
     // 1. Get the stock data for all periods
     getPeriod(formValues["ticker"], CONSTANTS.ONE_DAY, content.defaultStockIntervals[CONSTANTS.ONE_DAY]) // for (let period of Object.keys(content.defaultStockIntervals)) 
+    // 1.1 Get the SMA data for 1D period, using today's date
+    getSMA(formValues["ticker"], moment().format(CONSTANTS.DATE_FORMAT), CONSTANTS.ONE_DAY)
   }, [])
 
   // Subscribe to API Data to change the chart data properly for both onSubmit and onPageLoad
-  const chartDataUpdater = useEffect(() => {
+  useEffect(() => {
+    // 1.2 Get the SMA data for the other periods, using the dates provided by the API
+    // for each period, get sma data using form value's ticker and the date provided by the API data 
+
     const anonymous = async () => {
       // These are computed for one stock, but they are needed for all of them in backtester for it to work
-      const initiallyHolding = formValues.initHolding
-      const upperSell = formValues.upperIndicator
-      const lowerSell = formValues.lowerIndicator
+      const initiallyHolding = formValues.initHolding === "false" ? false : true;
+      const upperSell = parseFloat(formValues.upperSell)
+      const lowerSell = parseFloat(formValues.lowerSell)
 
-      // 1.1 Verify the API data exists before trying to use it
+      // 1.3 Verify the API data exists before trying to use it
       if (Object.values(APIData).every(periodAPIData => periodAPIData !== null && typeof periodAPIData !== 'undefined' && Object.values(periodAPIData).length) > 0) throw new Error(`APIData for a particular period is null, undefined, or is empty inside onPageLoad, verify you processed API data properly`)
 
       // 2. Process Raw API data into input consumable by the Backtester
-      console.log(APIData)
       const backtestedInputList = Object.keys(APIData).map(period => ({ [period]: Object.values(APIData[period]).length > 0 ? helpers.toBacktesterInput(APIData[period]) : [] })) // Convert API Data into backtester friendly input, for each period. return expected [{[period] : backtesterInput},...]
 
       // 2.1 Verify backtester output exists before using
       if (backtestedInputList === null || typeof backtestedInputList === 'undefined' || !Array.isArray(backtestedInputList)) throw new Error(`backtestedInput is ${backtestedInputList} inside onPageLoad, verify that the helper function works`)
 
-      console.log(backtestedInputList)
-      console.log(backtestedInputList[0])
-      console.log(Object.values(backtestedInputList[0])[0])
-      console.log(backtestedInputList[0]["1D"])
-      console.log(backtestedInputList[0]["1D"][0])
-      console.log(!!backtestedInputList[0]["1D"][0] ? backtestedInputList[0]["1D"][0]["date"] : "it is undefined")
       // 3. Pass backtester input to backtester to be backtested
-
-
-
-      // ----------------------------------  WHERE YOU LEFT OFF LAST TIME
-      //
-      // The main issue it seems here is that backtester is getting promises because getSMA returns a promise, and I can't await it 
-      // There will inevitably be more issues down the road, but this is the biggest one right now
-
-
       const backtestedOutputList = backtestedInputList.map(periodObj =>
       ({
         [Object.keys(periodObj)[0]]: Object.values(periodObj)[0].length > 0 && !!periodObj[Object.keys(periodObj)[0]][0] ?
           backtester(
             Object.values(periodObj)[0],
-            await getSmaDataPoint(formValues.ticker, periodObj[Object.keys(periodObj)[0]][0]["date"], 24),
-            await getSmaDataPoint(formValues.ticker, periodObj[Object.keys(periodObj)[0]][0]["date"], 12),
+            SMA[Object.keys(periodObj)[0]]["sma24"],
+            SMA[Object.keys(periodObj)[0]]["sma12"],
             initiallyHolding,
             upperSell,
             lowerSell)
@@ -141,18 +127,33 @@ export default function Home() {
       })) // return expected [{[period] : backtesterOutput},...]
 
       // 3.1 Verify backtested output exists and is a list with atleast one object before using
-      console.log(backtestedOutputList)
       if (backtestedOutputList === null || typeof backtestedOutputList === 'undefined' || !Array.isArray(backtestedOutputList) || Object.values(backtestedOutputList).length <= 0) throw new Error(`backtestedOutput is ${backtestedOutputList} inside onPageLoad, verify that the helper function works`)
+
+      // 3.2 Transform ugly input to useful form, then update the profit
+      const backtested = {} // initialize empty object
+      for (const dict of backtestedOutputList) {
+        const thePeriod = Object.keys(dict)[0]
+        const theValues = Object.values(dict)[0]
+        backtested[thePeriod] = Object.values(dict)[0].length > 0 ? theValues : []
+      }
+      const btLen = backtested[HighlightedPeriod].length - 1
+      const btInfo = backtested[HighlightedPeriod]
+      setProfit(btInfo != null && btLen > 0 ? btInfo[btLen]["current_profitability_multiplier"] : Profit) // Set the profit to be the last profit from the backtester
 
       // 4. Pass backtester output to the memo object maker function
       const memoObjList = backtestedOutputList.map(periodObj => ({ [Object.keys(periodObj)[0]]: Object.values(periodObj)[0].length > 0 ? helpers.toMemoObject(Object.values(periodObj)[0]) : [] })) // return expected [{[period] : memoObj},...]
 
-      console.log(memoObjList)
       // 4.1 Verify that the memoObject exists and has object keys
       if (memoObjList === null || typeof memoObjList === 'undefined' || Object.keys(memoObjList).length === 0) throw new Error(`memoObj is ${memoObjList} inside onPageLoad, verify that the helper function works`)
 
       // 5. Set Displayed to be the memoObject for each period the chart can display
-      for (let [index, period] of Object.keys(Displayed)) setDisplayed({ ...Displayed, [Object.keys(memoObjList[index])[0]]: Object.values(memoObjList[index])[0] }) // This assumes that the periods in the MemoObjList are all valid
+      const display = {} // initialize empty object
+      for (const dict of memoObjList) {
+        const thePeriod = Object.keys(dict)[0]
+        const theValues = Object.values(dict)[0]
+        display[thePeriod] = Object.values(dict)[0]["values"].length > 0 ? theValues : content.defaultDisplayed[thePeriod]
+      }
+      setDisplayed(display)
 
       // 5.1 Verify Displayed Visible Period Exists and has atleast 1 element
       if (Displayed[HighlightedPeriod] === null || typeof Displayed[HighlightedPeriod] === 'undefined' || Object.values(Displayed[HighlightedPeriod]).length === 0) throw new Error(`Displayed visible period is ${Displayed[HighlightedPeriod]} inside "Display" useState, check onPageLoad`)
@@ -164,8 +165,6 @@ export default function Home() {
       const visiblePeriodMemoObj = Object.values(Displayed[HighlightedPeriod])
       const len = visiblePeriodMemoObj[0].length
       setChartRange(len) // Set the chart range to match visible period length
-      setProfit(visiblePeriodMemoObj != null && len > 0 ? visiblePeriodMemoObj[0][len - 1] : Profit) // Set the profit to be the last profit from the backtester
-
     }
     anonymous()
   }, [APIData])
@@ -183,6 +182,8 @@ export default function Home() {
   const handleSubmit = e => {
     e.preventDefault()
     console.log(APIData)
+    console.log(SMA)
+    console.log(Displayed)
   }
 
   const onChange = e => setFormValues({ ...formValues, [e.target.name]: e.target.value })
@@ -200,13 +201,17 @@ export default function Home() {
           <h1 id="Site-Name">
             Basic Stock Backtester
           </h1>
+          <h1>range:{ChartRange}</h1>
           <section className="subtitle-container">
             <h2 id="Stock-Name">{formValues.ticker}</h2>
             <h3 id="Upper-Sell-Target">{`Upper Sell Target: ${formValues.upperSell}`}</h3>
             <h3 id="Lower-Sell-Target">{`Lower Sell Target: ${formValues.lowerSell}`}</h3>
           </section>
           <section className="chart-container">
-
+            <Line
+              options={content.options}
+              data={helpers.chartData(Object.keys(content.colors), helpers.range(0, ChartRange), Displayed[HighlightedPeriod], content.colors)}
+            />
             <ChartOptions>
               {content.chartStockPeriods.map((period) => (
                 <StockPeriodBtn
